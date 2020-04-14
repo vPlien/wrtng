a8036d360ee9eea7622b91250e82f229e691509c
Plien <leana@live.nl>
2020-04-14 17:53:54 +0200

    Inserted date and time, newlines and automatic end of file edit. Unfortunatly it overwrites the file when you run the command twice. (#8)
    Co-authored-by: vPlien <vplien@pm.me>

--- wrtng.sh
+++ wrtng.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -ev
+# Set to exit after non-zero status, and verbose
+set -ex
 
+# Define path for logs 
 WRTNG_PATH=~/.wrtng/$(date +%Y)/$(date +%m)
-
+# Make directory for path
 mkdir -p $WRTNG_PATH
 
-# open the log in vim with timestamps in natural language 
-vim ${WRTNG_PATH}/$(date +%d).txt
-
+# echo date and time to log
+echo -e "$(date +%A)" - "$(date +%B)" "$(date +%d)" - "$(date +%Y)" - ["$(date +%T)"] "\n\n" > ${WRTNG_PATH}/$(date +%d).txt
+# open vim in path with a new file per day
+vim "+normal G$" +startinsert ${WRTNG_PATH}/$(date +%d).txt
