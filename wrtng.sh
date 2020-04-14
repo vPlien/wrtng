05796b0541088fff03a0b18e2c472aaf5847ba45
Plien <leana@live.nl>
2020-04-14 19:54:14 +0200

    Fixed time and date and the overwriting issue (#10)
    Co-authored-by: vPlien <vplien@pm.me>

--- wrtng.sh
+++ wrtng.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Set to exit after non-zero status, and verbose
-set -ex
+# Set to exit after non-zero status
+set -e
+# for debugging comment out above one and use
+#set -ex
 
 # Define path for logs 
 WRTNG_PATH=~/.wrtng/$(date +%Y)/$(date +%m)
@@ -9,6 +11,6 @@ WRTNG_PATH=~/.wrtng/$(date +%Y)/$(date +%m)
 mkdir -p $WRTNG_PATH
 
 # echo date and time to log
-echo -e "$(date +%A)" - "$(date +%B)" "$(date +%d)" - "$(date +%Y)" - ["$(date +%T)"] "\n\n" > ${WRTNG_PATH}/$(date +%d).txt
+echo -e $(date "+%A - %B %d - %Y - [%T]")"\n\n" >> ${WRTNG_PATH}/$(date +%d).txt
 # open vim in path with a new file per day
 vim "+normal G$" +startinsert ${WRTNG_PATH}/$(date +%d).txt
