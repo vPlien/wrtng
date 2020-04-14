7d1520382ac52137a9e6f25ce3325d3e25170837
Plien <leana@live.nl>
2020-04-14 15:12:08 +0200

    simplified (#6)
    Co-authored-by: vPlien <vplien@pm.me>

--- wrtng.sh
+++ wrtng.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-export WRTNG_PATH=~/.wrtng
+set -ev
 
-mkdir -p $WRTNG_PATH
+WRTNG_PATH=~/.wrtng
 
-function wrtng(){
-	# if the log for today exists then
-    if [ ! -f ${WRTNG_PATH}/$(date +%Y-%V).txt ]; then
-		# enter the week and date line into the log for today
-        echo "Week $(date +"%V (%B %Y)") \n\n$(date +"%A %Y-%m-%d")" > ${WRTNG_PATH}/$(date +%Y-%V).txt
-    fi
-	# define the file edition date as the log with the stat function to see if the file has beet edited today
-    FILE_EDITION_DATE="$(stat -c "%y" ${WRTNG_PATH}/$(date +%Y-%V).txt)"
-	# define now as the date
-    NOW="$(date +"%Y-%m-%d")"
-	# if the file edition date (excluding the stat function) is not now then
-    if [ ${FILE_EDITION_DATE:0:10} != ${NOW} ]; then
-		# create a new logfile and enter the date in it
-        echo "\n$(date +"%A %Y-%m-%d")\n" >> ${WRTNG_PATH}/$(date +%Y-%V).txt
-    fi
-	# open the log in vim with timestamps in natural language 
-    vim +'normal Go' ${WRTNG_PATH}/$(date +%Y-%V).txt
-}
+# open the log in vim with timestamps in natural language 
+vim +'normal Go' ${WRTNG_PATH}/$(date +%Y-%V).txt
