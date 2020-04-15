5b7683c5d2758a144ae64af5822c921b39f14b0f
vPlien <vplien@pm.me>
2020-04-15 18:33:50 +0200

    commit

--- wrtng/wrtng.sh
+++ wrtng/wrtng.sh
@@ -11,6 +11,6 @@ WRTNG_PATH=~/.wrtng/$(date +%Y)/$(date +%m)
 mkdir -p $WRTNG_PATH
 
 # echo date and time to log
-echo -e $(date "+%A - %B %d - %Y - [%T]")"\n\n" >> ${WRTNG_PATH}/$(date +%d).txt
+echo -e "\n\n"$(date "+%A - %B %d - %Y - [%T]")"\n\n" >> ${WRTNG_PATH}/$(date +%d).txt
 # open vim in path with a new file per day
-vim "+normal G$" +startinsert ${WRTNG_PATH}/$(date +%d).txt
+vim "+normal Go" +startinsert ${WRTNG_PATH}/$(date +%d).txt
