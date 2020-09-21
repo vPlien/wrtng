d739c2170c810bb3e702f35ab743b37ce2bf69fa
Plien <leana@live.nl>
2020-09-22 00:10:55 +0200

    Implementation of argument parsing and multiple journal functionality (#21)
    * Implemented argument parsing
    
    * Rewrote echo to journal and opening vim to match the new variables
    
    Co-authored-by: vPlien <vplien@pm.me>

--- wrtng/wrtng.sh
+++ wrtng/wrtng.sh
@@ -1,16 +1,58 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# vim: ts=2 sts=2 sw=2 et
+
+# Enable debug mode if DEBUG=1
+if [[ $DEBUG == 1 ]]; then
+  set -x
+fi
 
 # Set to exit after non-zero status
-set -e
-# for debugging comment out above one and use
-#set -ex
+set -eu
 
-# Define path for logs 
-WRTNG_PATH=~/.wrtng/$(date +%Y)/$(date +%m)
-# Make directory for path
-mkdir -p $WRTNG_PATH
+# Journals directory
+WRTNG_DIR="$HOME/wrtng"
+mkdir -p "$WRTNG_DIR"
+
+# Heredoc https://tldp.org/LDP/abs/html/here-docs.html
+help_message() {
+  cat <<EOF
+Usage: $(basename "$0") [options] <journal>
+Journal keeping application with support for multiple journals
+
+Options:
+  --list        List all journals and exit
+EOF
+}
+
+UNPARSED=()
+# while number of arguments is greater then 0, do
+while [[ $# -gt 0 ]]; do
+  # select based on value of the first argument
+  case "$1" in
+    -l|--list)
+      list_journals
+      exit
+      ;;
+    *)
+      UNPARSED+="$1"
+      shift
+      ;;
+  esac
+done
+
+if [[ ${#UNPARSED[@]} -ne 1 ]]; then
+  echo 'Error: Invalid arguments...'
+  help_message
+  exit 1
+fi
+
+JOURNAL_NAME="${UNPARSED[0]}"
+
+# TODO: Do any journal name validation here
+
+JOURNAL="${WRTNG_DIR}/${JOURNAL_NAME}.md"
 
 # echo date and time to log
-echo -e "\n\n## "$(date "+%A %d/%m/%Y - [%T]")"\n\n" >> ${WRTNG_PATH}/$(date +%d).md
+echo -e "\n\n## "$(date "+%A %d/%m/%Y - [%T]")"\n\n" >> ${WRTNG_DIR}/${JOURNAL_NAME}.md
 # open vim in path with a new file per day
-vim "+normal Go" +startinsert ${WRTNG_PATH}/$(date +%d).md
+vim "+normal Go" +startinsert ${WRTNG_DIR}/${JOURNAL_NAME}.md
